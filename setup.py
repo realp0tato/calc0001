8c958a0fa650a9c05efd1fe4d5549842258e025c
realp0tato <136218419+realp0tato@users.noreply.github.com>
2025-06-02 15:28:57 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+
+import setuptools
+
+setuptools.setup(
+    name = 'ysh-calculator',
+    version = '0.0.1',
+    description='nice calculator',
+    author='yoonshi',
+    url = 'https://github.com/realp0tato/calc0001',
+    download_url= 'https://github.com/realp0tato/calc0001',
+    packages = [ 'mycalc'],
+    classifiers= [
+        "Programming Language :: Python :: 3",
+                  ]
+
+)
